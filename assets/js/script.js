af27d22d306aa59be43b45cb6ea6510b74986b7c
baptibrd <150129038+besty-boy@users.noreply.github.com>
2024-01-27 17:55:55 +0100

    update

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("cp").addEventListener('input', function () {
+    if (this.value.length === 5) {
+      
+                        let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=nom,code,codesPostaux,siren,codeEpci,codeDepartement,codeRegion,population&format=json&geometry=centre`;
+                        fetch(url).then((response) =>
+    response.json().then((data) => {
+                        let liste = "<ul>"
+                        for (let i of data) {
+                                            liste += `<li>${i.nom} - ${i.population} Habitants</li> <br />`;
+                        }
+                        liste += "</ul>";
+                        document.getElementById("content").innerHTML = liste;
+    })
+  );
+                        
+    }
+  });
+  
+  
